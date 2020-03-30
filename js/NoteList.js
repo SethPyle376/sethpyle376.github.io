9dea0e479420cbf3c75b8e33c589872e3afab630
Seth Pyle <seth.pyle86@gmail.com>
2020-03-30 11:27:40 -0600

    started note taking app

---
+++ js/NoteList.js
@@ -0,0 +1,35 @@
+function getNoteList() {
+    if (localStorage.getItem("notes")) {
+        return JSON.parse(localStorage.getItem("notes"))
+    } else {
+        return []
+    }
+}
+
+function createNote() {
+    var name = document.getElementById("newNote").value
+    localStorage.setItem(name, "")
+
+    var notes = getNoteList()
+    notes.push(name)
+    localStorage.setItem("notes", JSON.stringify(notes))
+    buildNoteList()
+}
+
+function updateNote(name, content) {
+    localStorage.setItem(name, content)
+}
+
+function buildNoteList() {
+    var content = "<table>"
+
+    var noteList = getNoteList()
+
+    noteList.forEach(note => {
+        content += "<tr><th><a href=\"/test.html\">" + note + "</a></th></tr>"
+    })
+
+    content += "</table>"
+
+    document.getElementById("noteList").innerHTML = content
+}
