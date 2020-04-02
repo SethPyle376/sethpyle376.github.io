eb7482859bf7f31423856df6f410dd5beea2f5e6
Seth Pyle <seth.pyle86@gmail.com>
2020-04-02 10:37:38 -0600

    pastebin stuff

--- js/NoteApp.js
+++ js/NoteApp.js
@@ -1,3 +1,8 @@
+const binKey = 'f4ff985f5f5baa336274aceaecb83c20'
+const binUrl = 'https://pastebin.com/api/api_post.php'
+
+
+
 function getNoteName() {
     const link = window.location.href
     var url = new URL(link);
@@ -19,4 +24,22 @@ function loadNote() {
 function saveNote() {
     var noteContent = document.getElementById("noteContent").value
     localStorage.setItem(getNoteName(), noteContent)
+}
+
+function binNote() {
+
+    const binOptions = {
+        api_dev_key: binKey,
+        api_option: 'paste',
+        api_paste_code: document.getElementById("noteContent").value,
+        api_paste_name: getNoteName(),
+        api_paste_private: 1,
+    }
+
+    fetch(binUrl, {
+        method: 'POST',
+        body: JSON.stringify(binOptions)
+    }).then(response => {
+        console.log(response)
+    })
 }
