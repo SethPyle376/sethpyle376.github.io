b2c2eb0dbf88979c46fd6fd3f8f4cabfdf9c9e68
Seth Pyle <seth.pyle86@gmail.com>
2020-04-02 10:05:50 -0600

    work on note app

--- js/NoteApp.js
+++ js/NoteApp.js
@@ -2,6 +2,21 @@ function getNoteName() {
     const link = window.location.href
     var url = new URL(link);
     var param = url.searchParams.get("note")
-    console.log(param)
     return param
+}
+
+function setNoteName() {
+    console.log(getNoteName())
+    document.getElementById("noteHeader").innerHTML = getNoteName()
+}
+
+function loadNote() {
+    setNoteName()
+    var note = localStorage.getItem(getNoteName())
+    document.getElementById("noteContent").value = note
+}
+
+function saveNote() {
+    var noteContent = document.getElementById("noteContent").value
+    localStorage.setItem(getNoteName(), noteContent)
 }
